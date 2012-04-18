6b76a8a65502523c463ea89b2834723da2e5389d
Benjamin Liles <benliles@arch.tamu.edu>
2012-04-18 13:05:49 -0500

    Removed unused submit_package_or_release view, closes #39

--- djangopypi/views/distutils.py
+++ djangopypi/views/distutils.py
@@ -19,61 +19,6 @@ log = getLogger('djangopypi.views.distutils')
 ALREADY_EXISTS_FMT = _(
     "A file named '%s' already exists for %s. Please create a new release.")
 
-def submit_package_or_release(user, post_data, files):
-    """Registers/updates a package or release"""
-    log.debug('submit_package_or_release(user=%s)' % (unicode(user),))
-    try:
-        package = Package.objects.get(name=post_data['name'])
-        if user not in package.owners.all():
-            log.error('%s is updating a package they don\'t own: %s' % 
-                      (unicode(user),unicode(package),))
-            return HttpResponseForbidden(
-                    "That package is owned by someone else!")
-    except Package.DoesNotExist:
-        log.info('%s is creating a new package: %s' % (unicode(user), unicode(post_data['name']),))
-        package = None
-
-    package_form = PackageForm(post_data, instance=package)
-    if package_form.is_valid():
-        package = package_form.save(commit=False)
-        package.owner = user
-        package.save()
-        for c in post_data.getlist('classifiers'):
-            classifier, created = Classifier.objects.get_or_create(name=c)
-            package.classifiers.add(classifier)
-        if files:
-            allow_overwrite = getattr(settings,
-                "DJANGOPYPI_ALLOW_VERSION_OVERWRITE", False)
-            try:
-                release = Release.objects.get(version=post_data['version'],
-                                              package=package,
-                                              distribution=UPLOAD_TO + '/' +
-                                              files['distribution']._name)
-                if not allow_overwrite:
-                    return HttpResponseForbidden(ALREADY_EXISTS_FMT % (
-                                release.filename, release))
-            except Release.DoesNotExist:
-                release = None
-
-            # If the old file already exists, django will append a _ after the
-            # filename, however with .tar.gz files django does the "wrong"
-            # thing and saves it as package-0.1.2.tar_.gz. So remove it before
-            # django sees anything.
-            release_form = ReleaseForm(post_data, files, instance=release)
-            if release_form.is_valid():
-                if release and os.path.exists(release.distribution.path):
-                    os.remove(release.distribution.path)
-                release = release_form.save(commit=False)
-                release.package = package
-                release.save()
-            else:
-                return HttpResponseBadRequest(
-                        "ERRORS: %s" % release_form.errors)
-    else:
-        return HttpResponseBadRequest("ERRORS: %s" % package_form.errors)
-
-    return HttpResponse()
-
 @basic_auth
 @transaction.commit_manually
 def register_or_upload(request):
